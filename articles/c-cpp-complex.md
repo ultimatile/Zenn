c0bf8bf6ae747a769c0d91d30e40eedaf1124ead
ultimatile <ultimatile@users.noreply.github.com>
2023-06-01 20:27:16 +0900

    [WIP] complex C++

--- articles/c-cpp-complex.md
+++ articles/c-cpp-complex.md
@@ -19,6 +19,15 @@ C++でCの複素数^[複素数体$\mathbb{C}$とC言語のCが紛らわしい感
 Cの`<complex.h>`で定義される複素数型はC++では使えないのでこれ以上触れません.
 このような事情があるので複素数型の変数をCの函数としてexternするには必然的にheaderless complexを用いる必要があります
 C++で複素数型を
+ISO C99で定義されているのは`float _Complex`, `double _Complex`, `long double _Complex`.
+ISO C99では`int _Complex`等の整数型は定義されていないがGCCではサポートされている
+
+- 異なる複素数型間の型変換が発生した場合, 対応する実数型の型変換規則に従う^[ISO C99でcorresponding real typeという用語が使われているので, 本記事でも対応する実数型と呼びます.].
+e.g., `float _Complex + double _Complex = double _Complex`
+- 実数型を複素数型に変換する場合は変換される実数型の値を実部に持ち,虚部が0の対応する複素数型になる.
+e.g., `float a`→`float _Complex (a,0)`
+- 複素数型を実数型に変換する場合は虚部は無視され, 実部を値として持つ対応する実数型に変換される.
+e.g.,`float _Complex (a,b)`→`float a`
 
 |言語|include|type|虚数単位|実部|虚部|
 |---|---|---|---|---|---|
@@ -28,3 +37,4 @@ C++で複素数型を
 
 http://nalab.mind.meiji.ac.jp/~mk/labo/text/complex-c.pdf
 https://cpprefjp.github.io/reference/complex/complex.html
+
--- programs/c-cpp-complex/complex.cpp
+++ programs/c-cpp-complex/complex.cpp
@@ -2,11 +2,22 @@
 #include <iostream>
 int main ()
 {
-    std::complex<double> c1(1.0, 2.0);
-    std::complex<double> c2(3.0, 4.0);
-    std::cout << c1 + c2 << std::endl;
-    float _Complex c3 = 1.0 + 2.0j;
-    float _Complex c4 = 2.0 + 4.0j;
-    std::cout << __real__ (c3 + c4) << "+" << __imag__ (c3 + c4) << "i" << std::endl;
+    //std::complex<double> c1(1.0, 2.0);
+    //std::complex<double> c2(3.0, 4.0);
+    //std::cout << c1 + c2 << std::endl;
+    //float _Complex c3 = 1.0 + 2.0j;
+    //float _Complex c4 = 2.0 + 4.0j;
+    //long double _Complex c5;
+    //_Imaginary long double c6;
+    //_Complex double z = 12.0 + 24.0i;
+    //using namespace std::literals::complex_literals;
+    //std::complex<double> w = 14.0 + 42.0i;
+    //std::cout << __real__ (c3 + c4) << "+" << __imag__ (c3 + c4) << "i" << std::endl;
+    //std::cout << __real__ z << "+" << __imag__ z << "i" << std::endl;
+    //std::cout << w.real() << "+" << w.imag() << "i" << std::endl;
+    _Complex double v = 12.0 + 24.0i;
+    //_Complex double* p_v = &v;
+    std::cout << __real__ v << "+" << __imag__ v << "i" << std::endl;
+    std::cout << *v << "+" << *v + 1 << "i" << std::endl;
     return 0;
 }
